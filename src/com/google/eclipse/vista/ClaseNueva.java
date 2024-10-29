867a4e0cd2b25574ad1f60ae14c2b2dc6b951dee
Luis Lopez <llvillar@gmail.com>
2024-10-29 11:34:25 +0100

    kjkjk

--- src/com/google/eclipse/vista/ClaseNueva.java
+++ src/com/google/eclipse/vista/ClaseNueva.java
@@ -5,6 +5,8 @@ public class ClaseNueva {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub
 
 	}
 
