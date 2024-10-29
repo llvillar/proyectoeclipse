e5a64efccd8b2ce8cdb40d11955e63b7e65b2c04
Luis Lopez <llvillar@gmail.com>
2024-10-29 10:32:27 +0100

    cambiosa en master

--- src/com/google/eclipse/vista/Hola.java
+++ src/com/google/eclipse/vista/Hola.java
@@ -3,7 +3,10 @@ package com.google.eclipse.vista;
 public class Hola {
 	
 	public static void main(String[] args) {
-		System.out.println("Hola mundo!!!!!");
+		System.out.println("Hola mundoooooooooooo!!!!!");
+		System.out.println("Hola mundoooooooooooo!!!!!");
+		System.out.println("Hola mundoooooooooooo!!!!!");
+		System.out.println("Hola mundoooooooooooo!!!!!");
 	}
 
 }
