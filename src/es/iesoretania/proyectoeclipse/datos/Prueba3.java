fdc58efe7f786d4e302f9656c63d0dcea76daabb
Luis Lopez <llopvil452@g.educaand.es>
2024-10-30 10:10:36 +0100

    cambio clase nueva

--- src/es/iesoretania/proyectoeclipse/datos/Prueba3.java
+++ src/es/iesoretania/proyectoeclipse/datos/Prueba3.java
@@ -3,8 +3,7 @@ package es.iesoretania.proyectoeclipse.datos;
 public class Prueba3 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print(false);
 	}
 
 }
