610891378f2070ef0023d481201fdf25825b3042
Luis Lopez <llvillar@gmail.com>
2024-11-05 09:54:53 +0100

    otro cambio en la clase nueva

--- src/com/google/eclipse/vista/ClaseNueva.java
+++ src/com/google/eclipse/vista/ClaseNueva.java
@@ -5,6 +5,8 @@ public class ClaseNueva {
 	public static void main(String[] args) {
 
 		System.out.println("Clase nueva");
+		System.out.println("Clase nueva");
+		System.out.println("Clase nueva");
 
 	}
 
